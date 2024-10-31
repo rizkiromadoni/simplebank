b3ef38542da6d065bb2d12157c2268dff12228f1
rizkiromadoni <rsahroromadoni@gmail.com>
2024-11-01 03:27:26 +0700

    feat: docker setup & fix typo in authorization key

--- api/middleware.go
+++ api/middleware.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	authorizationHeaderKey  = "uuthorization"
+	authorizationHeaderKey  = "authorization"
 	authorizationTypeBearer = "bearer"
 	authorizationPayloadKey = "authorization_payload"
 )
