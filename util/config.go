83337f93599e16027ca35e86ef15447366a2b723
rizkiromadoni <rsahroromadoni@gmail.com>
2024-11-04 16:43:48 +0700

    fix: docker and configuration

--- util/config.go
+++ util/config.go
@@ -20,6 +20,8 @@ func LoadConfig(path string) (config Config, err error) {
 	viper.SetConfigName("app")
 	viper.SetConfigType("env")
 
+	viper.AutomaticEnv()
+
 	err = viper.ReadInConfig()
 	if err != nil {
 		return
